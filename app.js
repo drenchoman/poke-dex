5afffb2417a55d3af084759cd0ff9d9f3c928892
Oscar Harron <oscarharron@hotmail.com>
2022-07-27 15:30:22 +1200

    add shiny update plus change gb color

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 let allPokemon = {}
-let app = document.getElementById('app')
 let searchTerm = ''
-let aSelect = document.getElementById('aSelect')
 let pokemonIndex = 0
+let imageIndex = 0
+let optionIndex = 0
+let app = document.getElementById('app')
+let body = document.getElementById('body')
+let shade = document.getElementById('shade')
+let aSelect = document.getElementById('aSelect')
 let bSelect = document.getElementById('bSelect')
 let bottomArrow = document.getElementById('direction-bottom')
 let topArrow = document.getElementById('direction-top')
-
-aSelect.addEventListener('click', getValueAndCallPokemon)
-bSelect.addEventListener('click', goBack)
-bottomArrow.addEventListener('click', function () {
-  moveBox('bottom')
-})
-topArrow.addEventListener('click', function () {
-  moveBox('top')
-})
+let rightArrow = document.getElementById('direction-right')
+let leftArrow = document.getElementById('direction-left')
+let startButton = document.getElementById('start')
 
 async function getPokemon() {
   let res = await fetch('https://pokeapi.co/api/v2/generation/generation-i')
@@ -38,16 +36,122 @@ function addPokemon(pokemons) {
     grid.appendChild(gridItem)
     app.appendChild(grid)
   })
+  addHomeListener()
+  addStartListener()
 }
 
 function capitaliseString(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+function addStartListener() {
+  startButton.addEventListener('click', addStartScreen)
+}
+
+function addHomeListener() {
+  rightArrow.addEventListener('click', moveBox)
+
+  leftArrow.addEventListener('click', moveBox)
+
+  aSelect.addEventListener('click', getValueAndCallPokemon)
+
+  bSelect.addEventListener('click', goBack)
+}
+
+function removeHomeListener() {
+  rightArrow.removeEventListener('click', moveBox)
+
+  leftArrow.removeEventListener('click', moveBox)
+
+  aSelect.removeEventListener('click', getValueAndCallPokemon)
+
+  console.log('removing')
+}
+
+function addPokemonListener() {
+  bottomArrow.addEventListener('click', moveBox)
+
+  topArrow.addEventListener('click', moveBox)
+
+  rightArrow.addEventListener('click', updatePokemonImage)
+
+  leftArrow.addEventListener('click', updatePokemonImage)
+}
+
+function removePokemonListener() {
+  rightArrow.removeEventListener('click', updatePokemonImage)
+
+  leftArrow.removeEventListener('click', updatePokemonImage)
+}
+
+function addStartScreenListener() {
+  rightArrow.addEventListener('click', changeTheme)
+  leftArrow.addEventListener('click', changeTheme)
+  aSelect.addEventListener('click', updateTheme)
+}
+
+function updateTheme() {
+  body.removeAttribute('class')
+  shade.removeAttribute('class')
+  body.classList.add('body')
+  body.classList.add(`theme-${optionIndex}-body`)
+  shade.classList.add('shade')
+  shade.classList.add(`theme-${optionIndex}-shade`)
+}
+
+function removeStartScreenListener() {
+  rightArrow.removeEventListener('click', changeTheme)
+  leftArrow.removeEventListener('click', changeTheme)
+  aSelect.removeEventListener('click', updateTheme)
+}
+
+function changeTheme() {
+  removeSelector(optionIndex)
+  let id = this.id
+  if (id == 'direction-right') {
+    optionIndex++
+    if (optionIndex == 6) {
+      optionIndex = 0
+    }
+    updateStartScreenSelector(optionIndex)
+  }
+  if (id == 'direction-left') {
+    optionIndex--
+    if (optionIndex == -1) {
+      optionIndex = 5
+    }
+    updateStartScreenSelector(optionIndex)
+  }
+}
+
+function updatePokemonImage() {
+  console.log(this.id)
+  let direction = this.id
+  let pokemon = allPokemon[pokemonIndex]
+  if (direction == 'direction-right') {
+    imageIndex++
+    if (imageIndex == 4) {
+      imageIndex = 0
+    }
+    updateImage(pokemon, imageIndex)
+  }
+  if (direction == 'direction-left') {
+    imageIndex--
+    if (imageIndex == -1) {
+      imageIndex = 3
+    }
+    updateImage(pokemon, imageIndex)
+  }
+}
+
+function updateImage(pokemon, index) {
+  document.getElementById(pokemon.name).src = pokemon.image[index]
+}
+
 function addCard(pokemon) {
   let div = createDiv('poke-card', pokemon.name, 'poke-header')
   let imageWrapper = createDiv('poke-image-wrapper')
-  let imageDiv = createImage(pokemon.image, pokemon.name)
+  let imageDiv = createImage(pokemon.image[0], pokemon.name)
   imageWrapper.appendChild(imageDiv)
   div.appendChild(imageWrapper)
 
@@ -61,7 +165,8 @@ async function getPokemonImage(allPokemon) {
     )
     let poke = await res.json()
     let image = await poke.sprites.front_default
-    pokemon.image = await image
+    pokemon.image = []
+    pokemon.image[0] = await image
   }
 }
 
@@ -122,13 +227,18 @@ function updatePokemon(
   allPokemon[index].height = height
   allPokemon[index].stats = stats
   allPokemon[index].moves = moves
-  allPokemon[index].sprites = sprites
+  allPokemon[index].image[1] = sprites.back_default
+  allPokemon[index].image[2] = sprites.front_shiny
+  allPokemon[index].image[3] = sprites.back_shiny
+  console.log(allPokemon[index])
   return allPokemon[index]
 }
 
 function showIndividualPokemon(pokemon) {
   app.textContent = ''
   addIndividualPokemonCard(pokemon)
+  removeHomeListener()
+  addPokemonListener(pokemon)
 }
 
 function addIndividualPokemonCard(pokemon) {
@@ -138,7 +248,7 @@ function addIndividualPokemonCard(pokemon) {
   let leftSide = createDiv('individual-left-side', pokemon.name, 'poke-header')
   let rightSide = createDiv('individual-right-side')
 
-  let imageDiv = createImage(pokemon.image, pokemon.name)
+  let imageDiv = createImage(pokemon.image[0], pokemon.name)
   let abilitiesList = createList(
     'Abilities:',
     pokemon.abilities,
@@ -170,6 +280,37 @@ function goBack() {
   addPokemon(allPokemon)
   app.classList.remove('individual-pokemon')
   resetBox(pokemonIndex)
+  removePokemonListener()
+  removeStartScreenListener()
+  addHomeListener()
+}
+
+function addStartScreen() {
+  app.textContent = ''
+  let div = createDiv('start-wrapper', 'Customise Gameboy', 'start-header')
+  let yellow = createImage('./assets/circle.svg', 'option-0')
+  let blue = createImage('./assets/circle.svg', 'option-1')
+  let green = createImage('./assets/circle.svg', 'option-2')
+  let purple = createImage('./assets/circle.svg', 'option-3')
+  let black = createImage('./assets/circle.svg', 'option-4')
+  let pattern = createImage('./assets/circle.svg', 'option-5')
+  div.append(yellow, blue, green, purple, black, pattern)
+  app.appendChild(div)
+  updateStartScreenSelector(optionIndex)
+  removeHomeListener()
+  removePokemonListener()
+  addStartScreenListener()
+}
+
+function removeSelector(index) {
+  let id = document.getElementById(`option-${index}`)
+  id.classList.remove('selected')
+}
+
+function updateStartScreenSelector(index) {
+  let id = document.getElementById(`option-${index}`)
+  console.log(id)
+  id.classList.add('selected')
 }
 
 function createImage(src, id) {
@@ -220,25 +361,26 @@ function createStat(string, stat) {
   return comboString
 }
 
-function moveBox(direction) {
-  if (direction === 'left') {
+function moveBox() {
+  let direction = this.id
+  if (direction === 'direction-left') {
     let grid = document.querySelector('.poke-grid')
     grid.scrollLeft -= 232
     updatePokemonIndex('dec')
   }
 
-  if (direction === 'right') {
+  if (direction === 'direction-right') {
     let grid = document.querySelector('.poke-grid')
     grid.scrollLeft += 232
     updatePokemonIndex('inc')
   }
 
-  if (direction == 'top') {
+  if (direction == 'direction-top') {
     let grid = document.getElementById('app')
     grid.scrollTop -= 50
   }
 
-  if (direction == 'bottom') {
+  if (direction == 'direction-bottom') {
     let grid = document.getElementById('app')
     grid.scrollTop += 50
   }
@@ -248,6 +390,7 @@ function resetBox(index) {
   let grid = document.querySelector('.poke-grid')
   let scrollAmount = index * 232
   grid.scrollLeft = scrollAmount
+  imageIndex = 0
 }
 
 function updatePokemonIndex(symbol) {
